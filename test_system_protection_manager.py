b0ba49978b6a93b770511912bb690cd68e6c6d03
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-07-16 14:01:57 +0200

    Modify test file

--- test_system_protection_manager.py
+++ test_system_protection_manager.py
@@ -1,6 +1,8 @@
 import json
 from unittest import mock
 
+import system_protection_manager
+
 # Importare le funzioni dallo script
 from system_protection_manager import (
     connect_to_node_manager,
@@ -13,24 +15,6 @@ from system_protection_manager import (
     publish_temperature,
     temperature_monitor,
 )
-
-"""
-# Test per la funzione on_message
-def test_on_message():
-    # Definire gli input di esempio per la funzione
-    ws = mock.Mock()
-    message = json.dumps({"Persistent": {"topic_name": "SIFIS:node-manager-id-ip-mapping"}})
-
-    # Chiamare la funzione e verificare l'output o il comportamento atteso
-    with mock.patch("builtins.print") as mock_print:
-        on_message(ws, message)
-
-    # Verificare che la funzione print sia stata chiamata con i messaggi corretti
-    assert mock_print.call_args_list == [
-        mock.call("Received:\n"),
-        mock.call({"Persistent": {"topic_name": "SIFIS:node-manager-id-ip-mapping"}}),
-    ]
-"""
 
 
 # Test per la funzione connect_to_node_manager
@@ -176,3 +160,135 @@ def test_on_error():
     # Definire gli input di esempio per la funzione
     ws = mock.Mock()
     error = "Error message"
+
+
+# Importare le funzioni dallo script
+from system_protection_manager import (
+    connect_to_node_manager,
+    dht_monitor,
+    on_close,
+    on_error,
+    on_message,
+    on_open,
+    publish_dht_data,
+    publish_temperature,
+)
+
+
+# Test per la funzione on_error
+def test_on_error():
+    ws = mock.Mock()
+    error = "Error message"
+    with mock.patch("builtins.print") as mock_print:
+        on_error(ws, error)
+        mock_print.assert_called_once_with(error)
+
+
+# Test per la funzione on_close
+def test_on_close():
+    ws = mock.Mock()
+    close_status_code = 1001
+    close_msg = "Connection closed"
+    with mock.patch("builtins.print") as mock_print:
+        on_close(ws, close_status_code, close_msg)
+        mock_print.assert_called_once_with("### Connection closed ###")
+
+
+# Test per la funzione on_open
+def test_on_open():
+    ws = mock.Mock()
+    with mock.patch("builtins.print") as mock_print:
+        on_open(ws)
+        mock_print.assert_called_once_with("### Connection established ###")
+
+
+# Test per la funzione connect_to_node_manager
+def test_connect_to_node_manager():
+    node_id = "node_id_example"
+    data = connect_to_node_manager(node_id)
+    expected_data = {
+        "RequestPostTopicUUID": {
+            "topic_name": "SIFIS:node-manager-kick-vote-sugg",
+            "topic_uuid": f"72b880d0fdc9a9a00dde4180727e908feb60e07bd614db710f606ca02f209153:{node_id}",
+            "value": {"kick": True, "time": 1234},
+        }
+    }
+    assert data == expected_data
+
+
+# Test per la funzione publish_dht_data
+def test_publish_dht_data():
+    dht_data = {"data": "example"}
+    with mock.patch("websocket.WebSocketApp") as mock_websocket:
+        publish_dht_data(dht_data)
+        expected_websocket_calls = [
+            mock.call(
+                "ws://146.48.62.99:3000/ws",
+                on_open=on_open,
+                on_message=on_message,
+                on_error=on_error,
+                on_close=on_close,
+            ),
+        ]
+        assert mock_websocket.call_args_list == expected_websocket_calls
+        assert mock_websocket.return_value.run_forever.call_args_list == [
+            mock.call(dispatcher=mock.ANY)
+        ]
+        assert mock_websocket.return_value.send.call_args_list == [
+            mock.call(json.dumps(dht_data))
+        ]
+
+
+# Test per la funzione publish_temperature
+def test_publish_temperature():
+    data = {"temperature": 25.5}
+    with mock.patch("websocket.WebSocketApp") as mock_websocket:
+        publish_temperature(data)
+        expected_websocket_calls = [
+            mock.call(
+                "ws://146.48.62.99:3000/ws",
+                on_open=on_open,
+                on_error=on_error,
+                on_close=on_close,
+            ),
+        ]
+        assert mock_websocket.call_args_list == expected_websocket_calls
+        assert mock_websocket.return_value.run_forever.call_args_list == [
+            mock.call(dispatcher=mock.ANY)
+        ]
+        assert mock_websocket.return_value.send.call_args_list == [
+            mock.call(json.dumps(data))
+        ]
+
+
+def test_dht_monitor():
+    dictionary = {"key": "value"}
+    requestor_id = "requestor_id_example"
+    request_id = "request_id_example"
+    data = dht_monitor(dictionary, requestor_id, request_id)
+    expected_data = {
+        "RequestPostTopicUUID": {
+            "topic_name": "SIFIS:Privacy_Aware_Device_DHT_inquiry",
+            "topic_uuid": "DHT_inquiry",
+            "value": {
+                "description": "DHT inquiry",
+                "requestor_id": requestor_id,
+                "request_id": request_id,
+                "requestor_type": "Pippo",
+                "connected": True,
+                "Data Type": "String",
+                "Dictionary": str(dictionary),
+            },
+        },
+    }
+    assert data == expected_data
+
+
+# Nuovo test per la funzione mock_websocket_send
+def test_mock_websocket_send():
+    data = {"key": "value"}
+    with mock.patch("websocket.WebSocketApp") as mock_websocket:
+        system_protection_manager.publish_dht_data(data)
+        mock_websocket.return_value.send.assert_called_once_with(
+            json.dumps(data)
+        )
